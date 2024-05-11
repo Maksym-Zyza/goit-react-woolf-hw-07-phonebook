76a0e8c6c78eafa550f3d1141675d2fce8b89b71
Maksym-Zyza <mmziza88@gmail.com>
2024-05-11 20:38:02 +0300

    added addMatcher

--- src/store/contacts/slice.js
+++ src/store/contacts/slice.js
@@ -1,57 +1,43 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { fetchApi } from 'API/api-service';
 
-// export const contactsAction = () => {
-//   return async dispatch => {
-//     try {
-//       dispatch(contactsSlice.actions.fetching());
-//       const data = await fetchApi.getContacts();
-//       dispatch(contactsSlice.actions.success(data));
-//     } catch (error) {
-//       dispatch(contactsSlice.actions.error(error));
-//     }
-//   };
-// };
-
-export const contactsAction = createAsyncThunk('contacts', async () => {
+export const contactsAction = createAsyncThunk('createContacts', async () => {
   const data = await fetchApi.getContacts();
   return data;
 });
+export const deleteAction = createAsyncThunk('deleteContacts', async id => {
+  const data = await fetchApi.deleteContact(id);
+  return data;
+});
+
+const handlePending = state => {
+  state.isLoading = true;
+  state.error = '';
+};
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.error.message;
+};
+const handleFulfilled = state => {
+  state.isLoading = false;
+};
 
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: { items: [], isLoading: false, error: null },
   extraReducers: builder => {
     builder
-      .addCase(contactsAction.pending, state => {
-        state.isLoading = true;
-        state.error = '';
-      })
       .addCase(contactsAction.fulfilled, (state, action) => {
-        state.isLoading = false;
         state.items = action.payload;
       })
-      .addCase(contactsAction.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload.message;
-      });
+      .addMatcher(action => action.type.endsWith('/pending'), handlePending)
+      .addMatcher(action => action.type.endsWith('/rejected'), handleRejected)
+      .addMatcher(
+        action => action.type.endsWith('/fulfilled'),
+        handleFulfilled
+      );
   },
 });
 
 export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
-
-// reducers: {
-//   fetching: state => {
-//     state.isLoading = true;
-//     state.error = '';
-//   },
-//   success: (state, action) => {
-//     state.isLoading = false;
-//     state.items = action.payload;
-//   },
-//   error: (state, action) => {
-//     state.isLoading = false;
-//     state.error = action.payload.message;
-//   },
-// },
